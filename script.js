1a1ebdb14eb306a22bc2ce6080add4c249a4e495
VB Student <vbstudent@uit.edu>
2018-05-24 17:03:21 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let a = 2;
+let b = 4.1;
+let c = 0;
+let d = true;
+let e = false;
+let firstName = "Muhammad";
+let lastName = "Yousuf";
+let greetings = "ramzan mubarak";
+let CarNo = "AUP-123";
+let carNo2 = "WER-124";
+let chair = null;
+let chair2;
+
+console.log (firstName)
+alert("chair")
